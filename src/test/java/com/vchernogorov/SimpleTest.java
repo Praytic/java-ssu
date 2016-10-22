76b26efe27793b84faadee346bdb2beb32c3baac
vchernogorov <vchernogorov@griddynamics.com>
2016-10-22 22:36:58 +0300

    Init commit

---
+++ src/test/java/com/vchernogorov/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.vchernogorov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
