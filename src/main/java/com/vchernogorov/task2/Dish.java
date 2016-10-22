6e88d66977d74bb1ee7e9e82964a1419fbbd2572
vchernogorov <vchernogorov@griddynamics.com>
2016-10-23 01:34:37 +0300

    Done Task2

---
+++ src/main/java/com/vchernogorov/task2/Dish.java
@@ -0,0 +1,46 @@
+package com.vchernogorov.task2;
+
+public class Dish {
+
+    private String name;
+    private int weight;
+    private int price;
+
+    public Dish() {
+    }
+
+    public Dish(String name, int weight, int price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + ", " + weight +" g., " + price + " rub.}";
+    }
+}
---
+++ src/main/java/com/vchernogorov/task2/Order.java
@@ -0,0 +1,47 @@
+package com.vchernogorov.task2;
+
+import java.util.List;
+
+public class Order {
+
+    private String customerName;
+    private List<Dish> dishes;
+
+    public Order() {
+    }
+
+    public Order(String customerName, List<Dish> dishes) {
+        this.customerName = customerName;
+        this.dishes = dishes;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public int getSummaryPrice() {
+        return dishes.stream().mapToInt(Dish::getPrice).sum();
+    }
+
+    @Override
+    public String toString() {
+        String string = "{" + customerName + ", ";
+        for (Dish dish : dishes) {
+            string += dish + ", ";
+        }
+        string += ", overall price: " + getSummaryPrice() + " rub.}";
+        return string;
+    }
+}
