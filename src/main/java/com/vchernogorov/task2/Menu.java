f4c3c4a7b031395dc0822a509a546afdb04f0b7d
vchernogorov <vchernogorov@griddynamics.com>
2016-10-23 01:34:37 +0300

    Done Task2

---
+++ src/main/java/com/vchernogorov/task2/Menu.java
@@ -0,0 +1,40 @@
+package com.vchernogorov.task2;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Menu {
+
+    private List<Dish> dishes;
+
+    public Menu() {
+    }
+
+    public Menu(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public Dish findDishByName(String name) {
+        return dishes.stream()
+                .filter(dish -> Objects.equals(dish.getName(), name))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    public String toString() {
+        String string = "{Name, Weight, Price}\n";
+        for (Dish dish : dishes) {
+            string += dish + "\n";
+        }
+        return string;
+    }
+}
